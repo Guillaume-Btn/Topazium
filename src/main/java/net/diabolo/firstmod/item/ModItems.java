f6f6595798014ecb613a4c234f7e944aaffdfba6
Diabolo335 <guillaumedelabuire@gmail.com>
2026-01-26 22:49:03 +0100

    Ajout du bismuth et du bismuth brut

--- src/main/java/net/diabolo/firstmod/item/ModItems.java
+++ src/main/java/net/diabolo/firstmod/item/ModItems.java
@@ -1,8 +1,19 @@
 package net.diabolo.firstmod.item;
 
 import net.diabolo.firstmod.FirstMod;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 public class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(FirstMod.MODID);
+    // Remplacez votre ligne actuelle par celle-ci :
+    public static final DeferredItem<Item> BISMUTH = ITEMS.registerSimpleItem("bismuth", props -> props);
+    public static final DeferredItem<Item> RAW_BISMUTH = ITEMS.registerSimpleItem("raw_bismuth", props -> props);
+
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
 }
