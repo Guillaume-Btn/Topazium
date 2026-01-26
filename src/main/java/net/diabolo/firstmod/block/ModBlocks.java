319f4a2a4340144bb3426e878a4febceb122e363
Diabolo335 <guillaumedelabuire@gmail.com>
2026-01-27 00:19:05 +0100

    Ajout du bloc de bismuth

---
+++ src/main/java/net/diabolo/firstmod/block/ModBlocks.java
@@ -0,0 +1,33 @@
+package net.diabolo.firstmod.block;
+
+
+import net.diabolo.firstmod.FirstMod;
+import net.diabolo.firstmod.item.ModItems;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredBlock;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+
+public class ModBlocks {
+    public static final DeferredRegister.Blocks BLOCKS= DeferredRegister.createBlocks(FirstMod.MODID);
+
+    public static final  DeferredBlock<Block> BISMUTH_BLOCK = BLOCKS.registerBlock(//seul methode qui marche
+            "bismuth_block",
+            Block::new, // factory qui reçoit les properties avec l'id déjà mis
+            BlockBehaviour.Properties.of()
+                    .strength(4.0f)
+                    .requiresCorrectToolForDrops()
+                    .sound(SoundType.AMETHYST)
+    );
+
+    static {
+        ModItems.ITEMS.registerSimpleBlockItem("bismuth_block", BISMUTH_BLOCK);
+    }
+
+    public static void register(IEventBus eventBus){
+        BLOCKS.register(eventBus);
+    }
+}
